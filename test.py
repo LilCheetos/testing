b3a72ad276fed49912107f9eaa84052102191301
LilCheetos <77062676+LilCheetos@users.noreply.github.com>
2023-01-02 18:27:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
-st.header('asdasd asda sd')
+st.title('Testing free host streamlit')
+st.header('This is a header')
+option = st.selectbox(
+    'Testing dropdown',
+    ('test1', 'test2', 'test3'))
+
+st.write('You selected:', option)
