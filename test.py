a5e194bfa6e54e5f19e65dedcc8de74d91bd8fae
LilCheetos <77062676+LilCheetos@users.noreply.github.com>
2023-01-02 18:48:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Testing free host streamlit')
-st.header('This is a header')
+st.title('Gay percentage test')
+st.header('Are u gay?')
 option = st.selectbox(
-    'Testing dropdown',
-    ('test1', 'test2', 'test3'))
+    'Select 1',
+    ('yes', 'definitely', 'YES but in capital'))
 
 st.write('You selected:', option)
